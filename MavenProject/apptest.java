9fc355eddbfb7fab16fe3fecd53bb94685723fd0
VENKATESH <90443397+Venkatvicky@users.noreply.github.com>
2025-04-02 11:08:37 +0530

    Add files via upload

---
+++ MavenProject/apptest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
